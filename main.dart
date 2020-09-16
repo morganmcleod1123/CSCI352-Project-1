5872f16f195274ce73bbb1d5e8609a0d33e35bb8
morganmcleod1123 <70032747+morganmcleod1123@users.noreply.github.com>
2020-09-16 12:51:57 -0500

    Update main.dart

--- main.dart
+++ main.dart
@@ -1,3 +1,5 @@
+import 'dart:async';
+
 import 'package:flutter/material.dart';
 
 void main() {
@@ -5,17 +7,20 @@ void main() {
 }
 
 class MyApp extends StatelessWidget {
+  bool tester = false;
   @override
   Widget build(BuildContext context) {
-    return MaterialApp(
-      title: 'Flutter Demo',
-      theme: ThemeData(
-
-        primarySwatch: Colors.green,
-        visualDensity: VisualDensity.adaptivePlatformDensity,
-      ),
-      home: MyHomePage(title: 'Recycling Plant Locator'),
-    );
+    if (tester == false) {
+      return MaterialApp(
+          title: 'Flutter Demo',
+          theme: ThemeData(
+            primarySwatch: Colors.green,
+            visualDensity: VisualDensity.adaptivePlatformDensity,
+          ),
+          home: MyHomePage()
+        //(title: 'Recycling Plant Locator'),
+      );
+    }
   }
 }
 
@@ -31,13 +36,11 @@ class MyHomePage extends StatefulWidget {
 
 class _MyHomePageState extends State<MyHomePage> {
   TextStyle _ts;
-  bool isSearching = false;
-
-
 
   @override
   Widget build(BuildContext context) {
     _ts = Theme.of(context).textTheme.headline4;
+
     return Scaffold(
       appBar: AppBar(
         title: Text("Recycling Plant Locator"),
@@ -52,21 +55,12 @@ class _MyHomePageState extends State<MyHomePage> {
         child: Column(
           mainAxisAlignment: MainAxisAlignment.center,
           children: <Widget>[
-            RaisedButton(
-              onPressed: () {},
-              child: Text("Recycling Plant 1", style: _ts)
+            Text("To search for a recycling plant based on material:\n\n"
+                "Search with the material code:\n\nPL(plastic)\nCA(cardboard)\n"
+                "GL(glass)\nPA(paper)\nor AL(Aluminum)\n\n"
+                "Make sure to fully type the name of the plant.", style: Theme.of(context).textTheme.headline5,
             ),
             Padding(padding: EdgeInsets.all(16.0),
-            ),
-            RaisedButton(
-                onPressed: () {},
-                child: Text("Recycling Plant 2", style: _ts)
-            ),
-            Padding(padding: EdgeInsets.all(16.0),
-            ),
-            RaisedButton(
-                onPressed: () {},
-                child: Text("Recycling Plant 3", style: _ts)
             ),
           ],
         ),
@@ -75,17 +69,31 @@ class _MyHomePageState extends State<MyHomePage> {
   }
 }
 
+RaisedButton buttonMaker(){
+  RaisedButton button = new RaisedButton(onPressed: null);
+  return button;
+}
+
 class DataSearch extends SearchDelegate<String>{
+  // https://www.youtube.com/watch?v=FPcl1tu0gDs&ab_channel=MTECHVIRAL
   final testing = [
-    "City of Conway Recycling Center",
-    "JSI Metal Recycling",
-    "ecoATM"
+    "PL: Conway Recycling Center",
+    "CA: Conway Recycling Center",
+    "GL: Conway Recycling Center",
+    "PA: Conway Recycling Center",
+    "AL: Conway Recycling Center",
+    "AL: JSI Metal Recycling",
+    "PL: Maumelle Recycling Center",
+    "CA: Maumelle Recycling Center",
+    "GL: Maumelle Recycling Center",
+    "PA: Maumelle Recycling Center",
+    "AL: Maumelle Recycling Center",
   ];
 
   final recentSearches = [
     "City of Conway Recycling Center",
     "JSI Metal Recycling",
-    "ecoATM"];
+    "Maumelle Recycling Center"];
 
   @override
   List<Widget> buildActions(BuildContext context) {
@@ -110,23 +118,51 @@ class DataSearch extends SearchDelegate<String>{
         close(context, null);
     }
     );
-    throw UnimplementedError();
   }
 
   @override
   Widget buildResults(BuildContext context) {
     // show some sort of result based on the actions
-    return Container(
-      height: 100.0,
-      width: 100.0,
-      child: Card(
-        color: Colors.green,
-        child: Center(
-          child: Text(query),
-        ),
+    if(query.substring(4, query.length) == "Conway Recycling Center"){
+      query = "City of Conway Recycling Center\n\n"
+          "Address: 4550 US-64, Conway, AR 72034\n\n"
+          "Hours: 7:30am - 3:30pm\n\n"
+          "Plastics, cardboard, glass, paper, and aluminum are all accepted here.";
+    }
+    else if (query.substring(4, query.length) == "JSI Metal Recycling"){
+      query = "JSI Metal Recycling\n\n"
+          "Address: 1605 E. Oak St.\n\n"
+          "Hours: 8:00am - 4:30pm\n\n"
+          "Aluminum and other metals are able to be recycled here.";
+    }
+    else if(query.substring(4, query.length) == "Maumelle Recycling Center"){
+      query = "Maumelle Recycling Center\n\n"
+          "Address: 600 Cogdell Drive\n\n"
+          "Hours: 7:00am - 4:00pm\n\n"
+          "Plastics, cardboard, glass, mixed paper, and aluminum are all accepted here.";
+    }
+    return Scaffold(
+      appBar: AppBar(
+        title: Text("Search Results")
       ),
-    );
-    throw UnimplementedError();
+      body: Center(
+        child: Column(
+          mainAxisAlignment: MainAxisAlignment.center,
+          children: <Widget>[
+            Container(
+              height: 535.0,
+              width: 400.0,
+              child: Card(
+                color: Colors.green,
+                child: Center(
+                  child: Text(query, style: Theme.of(context).textTheme.headline5),
+                ),
+              ),
+            )
+          ],
+          )
+        )
+      );
   }
 
   @override
@@ -148,7 +184,6 @@ class DataSearch extends SearchDelegate<String>{
     ),
       itemCount: suggestionList.length
     );
-    throw UnimplementedError();
   }
 
 }
